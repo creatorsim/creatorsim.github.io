5e6a0399496d53f254fe49175d41713047ce730f
Diego Camarmas Alonso <diego.camarmas.alonso@gmail.com>
2019-08-01 19:21:28 +0200

    1.0

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/********************
+ * Global variables *
+ ********************/
+
+
+
+/****************
+ * Vue instance *
+ ****************/
+
+
+window.app = new Vue({
+
+	/*DOM ID*/
+  el: "#app",
+
+
+  /*Vue data*/
+  data: {
+  	 
+  },
+
+
+  /*Created vue instance*/
+  created(){
+    
+  },
+
+
+  /*Mounted vue instance*/
+  mounted(){
+    
+  },
+
+
+  /*Vue methods*/
+  methods:{
+  	 
+  },
+});
+
+
+
+/*************
+ * Functions *
+ *************/
