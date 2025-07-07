8af8c0f2d4b5a3fb61a3156e8585ce67fb6b1c80
Diego Camarmas <dcamarma@inf.uc3m.es>
2025-07-07 13:30:10 +0200

    v5.0

---
+++ content/evolution/creator-5.0/examples/ag-uc3m-ec/asm-rv32_l3a_s53.s
@@ -0,0 +1,14 @@
+
+#
+# CREATOR (https://creatorsim.github.io/creator/)
+#
+
+.text
+ main: 
+
+         # t1 = (t0 < 0.0) ? 1 : 0
+
+         li   t0, -3
+         srli t1, t0, 31
+
+
---
+++ content/evolution/creator-5.0/test/mips/error/compiler/test_mips_error_compiler_018.s
@@ -0,0 +1,9 @@
+
+#
+# Creator (https://creatorsim.github.io/creator/)
+#
+
+.data
+  example:   .half 0x12345
+.text
+main: nop
